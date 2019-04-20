42b61ffd0af9473970139434f477eb7e6afced8f
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2019-04-20 20:54:45 +0100

    Add loaders for corresponding file types

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,31 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx/,
+        test: /\.js/,
         exclude: /node_modules/,
         loaders: ['babel-loader'],
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'url-loader?prefix=font/&limit=5000',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
     ]
   },
   plugins: [
