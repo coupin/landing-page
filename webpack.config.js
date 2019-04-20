4b81c6c626e77e9cacc0f840e2eec5a366236aff
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2019-04-20 10:35:56 +0100

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
@@ -22,12 +23,17 @@ module.exports = {
       title: 'Custom template',
       template: path.join(__dirname, 'src/index.html')
     }),
-    // new ScriptExtHtmlWebpackPlugin({
-    //   defaultAttribute: 'defer'
-    // })
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({}),
   ],
   stats: {
     colors: true
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    hot: true,
+    open: true,
+    disableHostCheck: true,
+  },
 };
