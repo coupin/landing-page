de4550fa51caabdbdc9a4d4e9c1f040c499cff34
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2019-10-18 07:21:25 +0100

    Add avatar to the testimonials

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -2,30 +2,35 @@ import template from 'lodash.template';
 
 var testimonials = [
   {
+    avatar: 'images/testimonial-avatar/wale.jpg',
     name: 'Wale H',
     comment: 'Coupin is easy to use and has a quick and easy layout. I recommend this.',
   },
   {
+    avatar: 'images/testimonial-avatar/ada.jpg',
     name: 'Ada',
     comment: 'Very impressive app, with the fast lifestyle and hustle and bustle in Lagos, it\'s easy to miss out on great deals. Great concept and execution.',
   },
   {
+    avatar: 'images/testimonial-avatar/anthony.jpg',
     name: 'Anthony',
     comment: 'Really useful app, now I can have access to my favourite deals on the go!',
   },
   {
+    avatar: 'images/testimonial-avatar/mike.jpg',
     name: 'Mike',
     comment: 'Fun to use and straight to the point. I get notifications of deals when I want them.',
   },
   {
-    name: 'Ralph',
+    avatar: 'images/testimonial-avatar/alex.jpg',
+    name: 'Alex',
     comment: 'A trusted app in finding just the right deals. I\'ll defintely recommend it to anyone.',
   }
 ];
 
 var testimonialString = '<div class="testimonial">' +
   '<div class="personal-info">' +
-  '<div class="avatar"></div>' +
+  '<img class="avatar" src="<%= avatar %>" alt="<%= name %> testimonial image">' +
   '<p class="name"><%= name %></p>' +
   '</div>' +
   '<p class="content"><%= comment %></p>' +
