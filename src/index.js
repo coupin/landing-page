81a6bfc177355a786bc3d7fd2af3f1a84f69c664
A.Oke-lawal <abiso.lawal@gmail.com>
2021-11-29 14:15:44 +0100

    - Minor changes

--- src/index.js
+++ src/index.js
@@ -16,13 +16,22 @@ import feature from './js/feature';
 
 feature.start();
 
+document.getElementById('merchant-dropdown').addEventListener('mouseenter', function() {
+  document.getElementById('merchant-dropdown-items').classList.add('show');
+});
+document.getElementById('merchant-dropdown').addEventListener('mouseleave', function() {
+  document.getElementById('merchant-dropdown-items').classList.remove('show')
+});
+
 window._scrollTo = function (id) {
   document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 };
 
 window.dismissModal = function () {
   document.querySelector('#lions-den-modal').classList.add('hidden');
-  document.querySelector('#youtube-frame').setAttribute('src', 'https://www.youtube.com/embed/rcm0XMAQ5As?autoplay=0&enablejsapi=1');
+  setTimeout(function() {
+    document.querySelector('#youtube-frame').setAttribute('src', 'https://www.youtube.com/embed/rcm0XMAQ5As?autoplay=0&enablejsapi=1');
+  }, 500);
 }
 
 import './index.html';
